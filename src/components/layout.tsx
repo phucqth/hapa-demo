61d29b0e5d2d83fe1008db5ceaca0cb21db7904d
IT-THUONG QUANG PHUC <thuongphuc97@gmail.com>
2025-02-23 00:20:43 +0700

    add hack  mobile nav bar fixed

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -6,6 +6,8 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
         <div className='bg-gradient-to-b from-blue-700 to-transparent sm:relative min-h-dvh'>
             <Header />
             <div className='p-4'>{children}</div>
+            <div className='h-20' />{' '}
+            {/* This is a hack to make the mobile nav bar fixed */}
             <MobileNav />
         </div>
     );
